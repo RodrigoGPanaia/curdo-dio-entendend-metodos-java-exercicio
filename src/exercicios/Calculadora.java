4a5fa9ff549e6cfb40d288a78c2a9f7a829e55fc
RodrigoGPanaia <rogapan@yahoo.com.br>
2022-04-29 10:48:47 -0300

    secund commit

--- src/exercicios/Calculadora.java
+++ src/exercicios/Calculadora.java
@@ -2,23 +2,27 @@ package exercicios;
 
 public class Calculadora {
 
-    public static double somar(double numero1, double numero2) {
-        return numero1 + numero2;
+    public static void somar(double numero1, double numero2) {
+
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + (numero1 + numero2));
+
     }
 
-    public static double subtrair(double numero1, double numero2) {
+    public static void subtrair(double numero1, double numero2) {
 
-        return numero1 - numero2;
+        System.out.println("A subtração de " + numero1 + " menos " + numero2 + " é " + (numero1 - numero2));
+
     }
 
-    public static double multiplicar(double numero1, double numero2) {
+    public static void multiplicar(double numero1, double numero2) {
 
-        return numero1 * numero2;
+        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é " + (numero1 * numero2));
+
     }
 
-    public static double dividir(double numero1, double numero2) {
+    public static void dividir(double numero1, double numero2) {
 
-        return numero1 / numero2;
+        System.out.println("A divisão de " + numero1 + " por " + numero2 + " é " + (numero1 / numero2));
     }
 
 }
--- src/exercicios/Principal.java
+++ src/exercicios/Principal.java
@@ -9,39 +9,37 @@ public class Principal {
         Scanner scan = new Scanner(System.in);
         double numero1, numero2;
 
+        System.out.println("Exercício calculadora");
+
         System.out.print("Digite o primeiro valor: ");
         numero1 = scan.nextDouble();
         System.out.print("Digite o segundo valor: ");
         numero2 = scan.nextDouble();
 
+        Calculadora.somar(numero1, numero2);
+        Calculadora.subtrair(numero1, numero2);
+        Calculadora.multiplicar(numero1, numero2);
+        Calculadora.dividir(numero1, numero2);
+
+        System.out.println("___________________________________________________________________________________");
         System.out.println();
 
-        System.out.println("Exercício calculadora");
+//        ------------------------------------------------------------------------------------
 
-        double somar = Calculadora.somar(numero1, numero2);
-        double subtrair = Calculadora.subtrair(numero1, numero2);
-        double multiplicar = Calculadora.multiplicar(numero1, numero2);
-        double dividir = Calculadora.dividir(numero1, numero2);
-
-
-        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + somar);
-        System.out.println("A subtração de " + numero1 + " menos " + numero2 + " é " + subtrair);
-        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é " + multiplicar);
-        System.out.println("A divisão de " + numero1 + " por " + numero2 + " é " + dividir);
-
-        System.out.println();
-
-        int hora;
         System.out.println("Exercício mensagem" );
+        int hora;
         System.out.print("Digite a hora do dia: ");
         hora = scan.nextInt();
 
         Mensagem.obterMensagem(hora);
 
+        System.out.println("___________________________________________________________________________________");
         System.out.println();
 
-        double valor;
+//        ------------------------------------------------------------------------------------
+
         System.out.println("Exercício empréstimo");
+        double valor;
         System.out.print("Digite o valor desejado R$ ");
         valor = scan.nextDouble();
 
@@ -50,7 +48,9 @@ public class Principal {
         Emprestimo.calcular((valor), Emprestimo.getQuatroParcelas());
         Emprestimo.calcular((valor), Emprestimo.getCincoParcelas());
         Emprestimo.calcular((valor), Emprestimo.getSeisParcelas());
-        Emprestimo.calcular((valor), 5);
+
+        System.out.println("___________________________________________________________________________________");
+        System.out.println();
 
     }
 
